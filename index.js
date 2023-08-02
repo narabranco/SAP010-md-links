43b36ee58b2e67d4216243d5aea1021e4222084e
narabranco <alice2013brandao@gmail.com>
2023-08-02 00:22:01 -0300

    exec no terminal console log

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.exports = () => {
-  // ...
-};
+//importando fileSystem para utilizar funcoes prontas
+//https://nodejs.org/dist/latest-v18.x/docs/api/fs.html#fsreadfilepath-options-callback
+const fs = require('fs');
+const filePath = './README.md';
+//funcao pronta do fileSystem que retorna o conteudo do arquivo ou o erro
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error("[*] Failed => ", err);
+    return;
+  }
+  console.log("[*] Sucess \n\n\n\n", data);
+});
